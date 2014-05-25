c0b5f612cadd927cfc1414c1b30a9867051a21bc
River Gillis <jrivergillis@gmail.com>
2014-05-24 20:28:14 -0500

    added trail mode

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -2,6 +2,11 @@ var square = '<div class="square"></div>';
 var divsToAppend;
 var numRows = 16;
 var numCols = 16;
+
+// due to the nature of floating point numbers, we have to remove a very
+// small amount of the height and width, since there can be times that
+// the numbers will be larger than the grid div, causing all sorts of
+// problems
 var width = 600 / numRows - 0.01;
 var height = 600 / numCols - 0.01;
 var mode = "pen";
@@ -10,6 +15,7 @@ $(document).ready(function() {
 	makeGrid();
 });
 
+// create a grid of divs and appends them to #grid
 function makeGrid() {
 	var divsToAppend = "";
 	for (i = 0; i < numCols; i++) {
@@ -24,6 +30,7 @@ function makeGrid() {
 	$('.square').css('width', width + 'px');
 }
 
+// draws to the grid depending on what mode is selected
 $(document).on('mouseenter', '.square', function() {
 	if (mode === "pen") {
 		$(this).css('background-color', 'black');
@@ -33,13 +40,20 @@ $(document).on('mouseenter', '.square', function() {
 		var opacity = parseFloat($(this).css("opacity"));
 		opacity += 0.1;
 		$(this).fadeTo("fast", opacity);
+	} else if (mode === "trail") {
+		$(this).fadeTo("fast", "1.0");
 	}
 });
 
 $(document).on('mouseleave', '.square', function() {
-
+	if (mode === "trail") {
+		$(this).fadeTo("fast", "0.1");
+	}
 });
 
+// destroys all of the divs in the grid and creates new divs
+// based on user input, calculates the correct width and heights for
+// these new divs and sets them, then changes the mode back to 'pen'
 function reset() {
 	$('.square').remove();
 	$('.row').remove();
@@ -53,6 +67,7 @@ function reset() {
 	mode = "pen";
 }
 
+// set the draw modes
 function setRandom() {
 	mode = "random";
 	$('.square').fadeTo("slow", "1.0");
@@ -68,6 +83,14 @@ function setFadeIn() {
 	$('.square').fadeTo("slow", "0.1");
 }
 
+function setTrail() {
+	mode = "trail";
+	$('.square').fadeTo("slow", "0.1");
+}
+
+// Returns a random color hex value as a string
+// Due to the pseudorandom  nature, it has a tendency
+// to pick darker colors
 function getRandomColor() {
 	var letters = '0123456789ABCDEF'.split('');
 	var color = '#';
