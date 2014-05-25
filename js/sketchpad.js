49b6607b193c170fa8cb0586ca82244896d8133e
River Gillis <jrivergillis@gmail.com>
2014-05-24 20:04:34 -0500

    added fade-in mode

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -29,6 +29,10 @@ $(document).on('mouseenter', '.square', function() {
 		$(this).css('background-color', 'black');
 	} else if (mode === "random") {
 		$(this).css('background-color', getRandomColor());
+	} else if (mode === "fadein") {
+		var opacity = parseFloat($(this).css("opacity"));
+		opacity += 0.1;
+		$(this).fadeTo("fast", opacity);
 	}
 });
 
@@ -39,21 +43,29 @@ $(document).on('mouseleave', '.square', function() {
 function reset() {
 	$('.square').remove();
 	$('.row').remove();
-	numRows = prompt("Please enter the number of rows");
-	numCols = prompt("Please enter the number of columns");
+	numRows = prompt("Please enter the number of rows", "16");
+	numCols = prompt("Please enter the number of columns", "16");
 	makeGrid();
 	width = 600 / numRows - 0.01;
 	height = 600 / numCols - 0.01;
 	$('.square').css('height', height + 'px');
 	$('.square').css('width', width + 'px');
+	mode = "pen";
 }
 
 function setRandom() {
 	mode = "random";
+	$('.square').fadeTo("slow", "1.0");
 }
 
 function setPen() {
 	mode = "pen";
+	$('.square').fadeTo("slow", "1.0");
+}
+
+function setFadeIn() {
+	mode = "fadein";
+	$('.square').fadeTo("slow", "0.1");
 }
 
 function getRandomColor() {
